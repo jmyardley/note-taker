99ea62249df1b8ea8e84673f85584ecd91d11d9a
James Y <35856061+jmyardley@users.noreply.github.com>
2020-12-05 17:12:24 -0500

    notes route

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const db = require ("./db/db.json");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello!');
 });
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use (express.static("public"));
+
+// Get '/notes'
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Get '*'
+
+// Get '/api.notes/'
+
+// POST '/api/notes'
+
+// DELETE '/api/notes/id'
+
+
+
 
 app.listen(PORT, () => {
     console.log("you've connected at http://localhost:" + PORT);
